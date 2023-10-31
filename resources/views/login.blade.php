e9a0d7bd4d2a64f874078172973a8ec6cc8083b6
nilacandrapuspita <116093596+nilacandrapuspita@users.noreply.github.com>
2023-10-31 15:17:42 +0700

    mendaftar

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,11 +1,26 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+	<title>FORM LOGIN PPDB - SMA NEGERI 7 YOGYAKARTA</title>	
 </head>
 <body>
-    hallo
+	<h1>SMA NEGERI 7 YOGYAKARTA</h1>
+	<h3>Aplikasi PPDB Online</h3>
+	<form action="login.php" method="post">		
+		<table>
+			<tr>
+				<td>USERNAME</td>
+				<td><input type="text" name="username"></td>
+			</tr>
+			<tr>
+				<td>PASSWORD</td>
+				<td><input type="password" name="password"></td>
+			</tr>
+			<tr>
+				<td></td>
+				<td><input type="submit" name="login" value="Log In"></td>
+			</tr>
+		</table>
+	</form>
 </body>
 </html>
