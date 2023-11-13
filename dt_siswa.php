03b98509e3e94b168083675609381a511d5ffb57
SalsabilaWaf <133813883+SalsabilaWaf@users.noreply.github.com>
2023-11-13 14:48:52 +0700

    add data siswa

--- dt_siswa.php
+++ dt_siswa.php
@@ -1,15 +1,66 @@
-<!DOCTYPE HTML>
+<?php
+?>
+
+<!DOCTYPE html>
 <html>
+
+<head>
+    <title>CEK STATUS PENDAFTARAN ANDA - SMA NEGERI 7 YOGYAKARTA</title>
+</head>
 
 <body>
 
-  <p>Before the script...</p>
+    <header class="tz-header tz-header-2">
+        <div class="tz-sign-in">
+            <div class="container">
+                <p>SELAMAT DATANG DI APLIKASI PPDB ONLINE SMA NEGERI 7 YOGYAKARTA</p>
+            </div>
+        </div>
+        <div class="tz-customer-container">
+            <div class="container">
+                <div class="tz-header-content">
+                    <h3 class="tz-logo pull-left">
+                        <a href="#"><img alt="Images Logo" src=""></a>
+                    </h3>
+                    <button type="button" class="tz-button-toggle btn-navbar pull-right" data-target=".nav-collapse">
+                        <i class="fa fa-bars"></i>
+                    </button>
+                    <nav class="nav-collapse pull-right">
+                        <ul class="tz-menu">
+                            <li><a href="index">HOME</a></li>
+                            <li><a href="mendaftar#fc">MENDAFTAR</a></li>
+                            <li><a href="dt_siswa">DATA CALON SISWA</a></li>
+                            <li><a href="grafik#fc">GRAFIK</a></li>
+                            <li><a href="login#fc">LOGIN</a></li>
+                        </ul>
+                    </nav>
+                </div>
+            </div><!-- End Content Main Header -->
+        </div>
+    </header>
+    <!-- End class tz-header-2 -->
 
-  <script>
-    alert( 'Hello, world!' );
-  </script>
-
-  <p>...After the script.</p>
+    <section class="tz-banner tz-banner-style2">
+        <!-- Begin class banner style-3 -->
+        <div class="tz-banner-style2">
+            <div class="tz-slider-banner">
+                <div class="tz-items">
+                    <div class="tz-slider-images">
+                        <img src="demos/Banner/banner-03.jpg" alt="Images">
+                    </div>
+                    <div class="tz-banner-content">
+                        <div class="container">
+                            <!-- <small>NUMBER ONE</small> -->
+                            <h4>SMA NEGERI 7 YOGYAKARTA</h4>
+                            <span class="tz-under-line"></span>
+                            <h6>Aplikasi PPDB Online</h6>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- End  class banner style-3 -->
+    </section>
 
 </body>
 
