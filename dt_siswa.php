f4863f62a36e1d2b982ca7e23722de8aab151d7c
SalsabilaWaf <133813883+SalsabilaWaf@users.noreply.github.com>
2023-11-21 13:07:56 +0700

    add data siswa

--- dt_siswa.php
+++ dt_siswa.php
@@ -13,6 +13,29 @@ include("con_db/connection.php");
 </head>
 
 <body>
+<header class="tz-header tz-header-2">
+        <div class="tz-sign-in">
+            <div class="container">
+                <p>SELAMAT DATANG DI APLIKASI PPDB ONLINE SMA NEGERI 7 YOGYAKARTA</p>
+            </div>
+        </div>
+        <div class="tz-customer-container">
+            <div class="container">
+                <div class="tz-header-content">
+                    <h3 class="tz-logo pull-left">
+                        <a href="#"><img alt="Images Logo" src=""></a>
+                    </h3>
+                    <button type="button" class="tz-button-toggle btn-navbar pull-right" data-target=".nav-collapse">
+                        <i class="fa fa-bars"></i>
+                    </button>
+                    <?php
+                    include("");
+                    ?>
+                </div>
+            </div><!-- End Content Main Header -->
+        </div>
+    </header>
+    <!-- End class tz-header-2 -->
 </body>
 
 </html>
