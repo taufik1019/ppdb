75f3b0abeeb665e776af970ca26cb1c8ea5112dc
SalsabilaWaf <133813883+SalsabilaWaf@users.noreply.github.com>
2023-11-21 13:03:53 +0700

    add data siswa

--- dt_siswa.php
+++ dt_siswa.php
@@ -1,99 +1,18 @@
 <?php
+session_start();
+include("con_db/connection.php");
 ?>
 
 <!DOCTYPE html>
 <html>
 
 <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>CEK STATUS PENDAFTARAN ANDA - SMA NEGERI 7 YOGYAKARTA</title>
 </head>
 
 <body>
-
-    <header class="tz-header tz-header-2">
-        <div class="tz-sign-in">
-            <div class="container">
-                <p>SELAMAT DATANG DI APLIKASI PPDB ONLINE SMA NEGERI 7 YOGYAKARTA</p>
-            </div>
-        </div>
-        <div class="tz-customer-container">
-            <div class="container">
-                <div class="tz-header-content">
-                    <h3 class="tz-logo pull-left">
-                        <a href="#"><img alt="Images Logo" src=""></a>
-                    </h3>
-                    <button type="button" class="tz-button-toggle btn-navbar pull-right" data-target=".nav-collapse">
-                        <i class="fa fa-bars"></i>
-                    </button>
-                    <nav class="nav-collapse pull-right">
-                        <ul class="tz-menu">
-                            <li><a href="index">HOME</a></li>
-                            <li><a href="mendaftar#fc">MENDAFTAR</a></li>
-                            <li><a href="dt_siswa">DATA CALON SISWA</a></li>
-                            <li><a href="grafik#fc">GRAFIK</a></li>
-                            <li><a href="login#fc">LOGIN</a></li>
-                        </ul>
-                    </nav>
-                </div>
-            </div><!-- End Content Main Header -->
-        </div>
-    </header>
-    <!-- End class tz-header-2 -->
-
-    <section class="tz-banner tz-banner-style2">
-        <!-- Begin class banner style-3 -->
-        <div class="tz-banner-style2">
-            <div class="tz-slider-banner">
-                <div class="tz-items">
-                    <div class="tz-slider-images">
-                        <img src="demos/Banner/banner-03.jpg" alt="Images">
-                    </div>
-                    <div class="tz-banner-content">
-                        <div class="container">
-                            <!-- <small>NUMBER ONE</small> -->
-                            <h4>SMA NEGERI 7 YOGYAKARTA</h4>
-                            <span class="tz-under-line"></span>
-                            <h6>Aplikasi PPDB Online</h6>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!-- End  class banner style-3 -->
-    </section>
-    
-    <!-- Begin section tz-introduce-univesity -->
-    <section class="tz-introduce-univesity">
-        <!-- Start class tzWrap -->
-        <div class="tz-cource-services tz-cource-services-style-2">
-            <ul>
-                <li>
-                    <div class="tz-background-color-1">
-                        <a href="mendaftar#fc"><i class="tz-color-1 fa fa-file-text-o"></i></a>
-                        <span><a href="mendaftar#fc">MENDAFTAR</a></span>
-                    </div>
-                </li>
-                <li>
-                    <div class="tz-background-color-2">
-                        <a href="dt_siswa"><i class="tz-color-2 fa fa-paperclip"></i></a>
-                        <span><a href="dt_sw">DATA SISWA</a></span>
-                    </div>
-                </li>
-                <li>
-                    <div class="tz-background-color-3">
-                        <a href="grafik#fc"><i class="tz-color-3 fa fa-magic"></i></a>
-                        <span><a href="grafik">GRAFIK</a></span>
-                    </div>
-                </li>
-                <li>
-                    <div class="tz-background-color-4">
-                        <a href="login#fc"><i class="tz-color-4 fa fa-cog"></i></a>
-                        <span><a href="login#fc">LOGIN</a></span>
-                    </div>
-                </li>
-            </ul>
-        </div>
-        <div class="tzWrap">
 </body>
 
 </html>
