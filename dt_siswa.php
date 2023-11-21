5626360f6faefeac0741162c9da9dc922456ad17
SalsabilaWaf <133813883+SalsabilaWaf@users.noreply.github.com>
2023-11-21 12:42:23 +0700

    add data siswa

--- dt_siswa.php
+++ dt_siswa.php
@@ -61,7 +61,39 @@
         </div>
         <!-- End  class banner style-3 -->
     </section>
-
+    
+    <!-- Begin section tz-introduce-univesity -->
+    <section class="tz-introduce-univesity">
+        <!-- Start class tzWrap -->
+        <div class="tz-cource-services tz-cource-services-style-2">
+            <ul>
+                <li>
+                    <div class="tz-background-color-1">
+                        <a href="mendaftar#fc"><i class="tz-color-1 fa fa-file-text-o"></i></a>
+                        <span><a href="mendaftar#fc">MENDAFTAR</a></span>
+                    </div>
+                </li>
+                <li>
+                    <div class="tz-background-color-2">
+                        <a href="dt_siswa"><i class="tz-color-2 fa fa-paperclip"></i></a>
+                        <span><a href="dt_sw">DATA SISWA</a></span>
+                    </div>
+                </li>
+                <li>
+                    <div class="tz-background-color-3">
+                        <a href="grafik#fc"><i class="tz-color-3 fa fa-magic"></i></a>
+                        <span><a href="grafik">GRAFIK</a></span>
+                    </div>
+                </li>
+                <li>
+                    <div class="tz-background-color-4">
+                        <a href="login#fc"><i class="tz-color-4 fa fa-cog"></i></a>
+                        <span><a href="login#fc">LOGIN</a></span>
+                    </div>
+                </li>
+            </ul>
+        </div>
+        <div class="tzWrap">
 </body>
 
 </html>
