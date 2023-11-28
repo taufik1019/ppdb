61d6b31ba2eecb242771dfa9baffda40df21df12
SalsabilaWaf <133813883+SalsabilaWaf@users.noreply.github.com>
2023-11-28 15:35:17 +0700

    update index

---
+++ admin/index.php
@@ -0,0 +1,22 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="utf-8">
+	<title>Panel Dashboard</title>
+    <meta name="keywords" content="Panel Dashboard" />
+    <meta name="description" content="Panel Dashboard">
+	<meta name="author" content="Panel Dashboard">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body class="dashboard-page sb-l-o sb-r-c">
+
+				    
+</body>
+
+</html>
+
