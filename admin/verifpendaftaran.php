4510b6e1c69070e1967a761adfdba46177b021db
Putri Kaligis <87135234+pute1312@users.noreply.github.com>
2023-11-29 13:03:09 +0700

    add verif pendaftaran

---
+++ admin/verifpendaftaran.php
@@ -0,0 +1,72 @@
+<?php
+session_start();
+if(isset($_SESSION['fi_id']) && isset($_SESSION['fi_us']) && isset($_SESSION['fi_ps']))
+    {
+        include ("../con_db/connection.php");
+        error_reporting("null");
+        $fi_id=$_SESSION['fi_id'];
+        $status=$_SESSION['fi_st'];
+        if($status=="Admin")
+            {
+                ?>
+                 <!DOCTYPE html>
+                <html>
+
+                <head>
+                    <!-- Meta, title, CSS, favicons, etc. -->
+                   <meta charset="utf-8">
+                    <title>Verifikasi Data Pendaftar</title>
+                    <meta name="keywords" content="Verifikasi Data Pendaftar" />
+                    <meta name="description" content="Verifikasi Data Pendaftar">
+                    <meta name="author" content="Verifikasi Data Pendaftar">
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+                    <!-- Required Plugin CSS -->
+                    <link rel="stylesheet" type="text/css" href="assets/js/utility/highlight/styles/googlecode.css">
+
+                    <!-- Required Plugin CSS -->
+                    <link rel="stylesheet" type="text/css" href="vendor/plugins/datepicker/css/bootstrap-datetimepicker.css">
+                    <link rel="stylesheet" type="text/css" href="vendor/plugins/daterange/daterangepicker.css">
+                    <link rel="stylesheet" type="text/css" href="vendor/plugins/colorpicker/css/bootstrap-colorpicker.min.css">
+                    <link rel="stylesheet" type="text/css" href="vendor/plugins/tagmanager/tagmanager.css">
+
+                    <!-- Theme CSS -->
+                    <link rel="stylesheet" type="text/css" href="assets/skin/default_skin/css/theme.css">
+
+                    <link rel="stylesheet" type="text/css" href="assets/admin-tools/admin-forms/css/admin-forms.css">
+                    <!-- Favicon -->
+                    <link rel="stylesheet" type="text/css" href="vendor/plugins/datatables/media/css/dataTables.bootstrap.css">
+                    <link rel="shortcut icon" href="assets/img/favicon.ico">
+
+                    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+                    <!--[if lt IE 9]>
+                    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+                    <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+                    <![endif]-->
+                </head>
+                
+                </html>
+
+
+
+<?php
+            }
+        else
+            {
+                ?>
+                <script type="text/javascript">
+                    window.location="../index";
+                </script>
+                <?php
+            }
+    }
+else
+    {
+        ?>
+        <script type="text/javascript">
+            window.location="../index";
+        </script>
+        <?php
+    }
+?>
+
