ae15452310d06033f35cff33be9865f787e4dc6d
Putri Kaligis <87135234+pute1312@users.noreply.github.com>
2023-12-06 23:00:14 +0700

    add verif pendaftar

--- admin/verifpendaftaran.php
+++ admin/verifpendaftaran.php
@@ -458,7 +458,70 @@ if(isset($_SESSION['fi_id']) && isset($_SESSION['fi_us']) && isset($_SESSION['fi
                         <!-- Start: Right Sidebar -->
                         
                         <!-- End: Right Sidebar -->
+
                     </div>
+                     <!-- End: Main -->
+                     <script type="text/javascript">  
+                            function addRow(tableID) {
+
+                                var table = document.getElementById(tableID);
+
+                                var rowCount = table.rows.length;
+                                var row = table.insertRow(rowCount);
+
+                                var colCount = table.rows[0].cells.length;
+
+                                for (var i = 0; i < colCount; i++) {
+
+                                    var newcell = row.insertCell(i);
+
+                                    newcell.innerHTML = table.rows[0].cells[i].innerHTML;
+                                    //alert(newcell.childNodes);
+                                    switch (newcell.childNodes[0].type) {
+                                        case "text":
+                                            newcell.childNodes[0].value = "";
+                                            break;
+                                        case "checkbox":
+                                            newcell.childNodes[0].checked = true;
+                                            break;
+                                        case "select-one":
+                                            newcell.childNodes[0].selectedIndex = 0;
+                                            break;
+                                        case "radio":
+                                            newcell.childNodes[0].checked= false;
+                                            break;
+                                        case "textarea":
+                                            newcell.childNodes[0].selectedIndex = 0;
+                                            break;
+                                    }
+
+                                }
+                            }
+
+                            function deleteRow(tableID) {
+                                try {
+                                    var table = document.getElementById(tableID);
+                                    var rowCount = table.rows.length;
+
+                                    for (var i = 0; i <rowCount; i++) {
+                                        var row = table.rows[i];
+                                        var chkbox = row.cells[0].childNodes[0];
+                                        if (null != chkbox && true == chkbox.checked) {
+                                            if (rowCount <= 1) {
+                                                alert("Tidak Dapat Mengapus Semua Row");
+                                                break;
+                                            }
+                                            table.deleteRow(i);
+                                            rowCount--;
+                                            i--;
+                                        }
+                                    }
+                                } catch (e) {
+                                    alert(e);
+                                }
+                            }
+
+                        </script>
                 </body>
                 </html>
 
